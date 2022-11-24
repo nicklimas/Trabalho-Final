64b534e2cf213c3b68dcb8e7300d9d6a72181672
SamanthaKelly01 <samanthak2002@hotmail.com>
2022-11-24 13:47:11 -0300

    Atualizando cadastros.hpp

---
+++ include/biblioteca/cadastros.hpp
@@ -0,0 +1,16 @@
+#ifndef CADASTROS_H
+#define CADASTROS_H
+
+#include "sistema.hpp"
+
+class Cadastros : public Sistema {
+    
+    
+    public:
+        std::string login ();
+        void adciona ();
+
+};
+
+
+#endif
