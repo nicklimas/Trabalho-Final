ea96913102c1ca2b94b18f61c22c6fb24a1632d2
SamanthaKelly01 <samanthak2002@hotmail.com>
2022-11-25 14:55:25 -0300

    Atualizando sistema.hpp

---
+++ include/biblioteca/sistema.hpp
@@ -0,0 +1,29 @@
+#ifndef SISTEMA_H
+#define SISTEMA_H
+
+#include "../livro/livro.hpp"
+#include "../pessoas/usuario.hpp"
+#include "../pessoas/admin.hpp"
+#include "../erros/erros.hpp"
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+class Sistema {
+
+
+    
+    void adciona ();
+    std::string login ();
+
+
+
+
+};
+
+
+#endif
