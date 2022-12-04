ef83490240aa61f801954f4d4f7b5a9dc9bfaa9c
ArthuVilela <112183761+ArthuVilela@users.noreply.github.com>
2022-12-04 16:09:16 -0300

    finalizando testes da biblioteca

--- tests/teste_biblioteca.hpp
+++ tests/teste_biblioteca.hpp
@@ -15,12 +15,12 @@ TEST_CASE("Testando Biblioteca: ") {
     //Adiciona exclui livro
 
     Livro *livro = new Livro;
-    livro = b.adciona_livro();
+    livro = b.adciona_livro(); //adiciona livro
     //entradas.txt
-    CHECK(livro->get_titulo() == "testando");
-    CHECK(b.find_livro(livro->get_titulo())== livro);
-    b.remove_livro(livro);
-    CHECK_FALSE(livro->get_titulo() == "testando");
+    CHECK(livro->get_titulo() == "testando"); // get titulo test
+    CHECK(b.find_livro(livro->get_titulo())== livro); //confere se adicionou
+    b.remove_livro(livro); // remove livro
+    CHECK_FALSE(livro->get_titulo() == "testando"); //confere se remooveu
 
 
     //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
@@ -41,56 +41,7 @@ TEST_CASE("Testando Biblioteca: ") {
     //cat = (Biblioteca::_categoria.find("CatTest")) ;
     CHECK(autor == "CatTest");
 
-
-
-
-    /*
-        //---Testa se tem cout---\\
-        std::ostringstream oss;
-        std::streambuf* p_cout_streambuf = std::cout.rdbuf();
-        std::cout.rdbuf(oss.rdbuf());
-
-        f();
-
-        std::cout.rdbuf(p_cout_streambuf); // restore
-
-        // test your oss content...
-        assert(oss && oss.str() == "hello world\n";
-        std::cout << oss.str();
-    */
  
    
 }
 
-/*
-    Lista de funcoes
-
-
-    void inicia_livros ();
-    void inicia_categorias ();
-    void inicia_autores ();
-
-    //  Gets
-    std::map <std::string, Livro*>* get_livros ();
-
-    // Métodos de cout
-    void lista_livros ();
-    void lista_autores ();
-    void lista_livros_do_autores (std::string autor);
-    void lista_categorias ();
-    void lista_livros_da_categoria (std::string categoria);
-
-    // Metodos que atualizam arquivos de salvamento
-        void add_autor (std::string autor, std::string titulo);
-        void add_categoria (std::string categoria, std::string titulo);
-
-    // Metodos de criação e deleção de livros do sistema da biblioteca
-        Livro* adciona_livro ();
-        void remove_livro(Livro* livro);
-
-    // Metodo para achar um livro dentro do sistema
-        Livro* find_livro (std::string pesquisa);  
-
-
-//=======
-*/
